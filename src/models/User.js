8e6ff107bd9583a007e773f008329d7fd27de51d
Aarón RJ <aaron@quantumstudio.es>
2020-03-10 08:44:22 +0100

    Commit #1 10/03/2020 - Creados los modelos de datos de MongoDB. Se usarán para dar de alta los datos en la BD.

---
+++ src/models/User.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+
+}, { timestamps: true });
+
+UserSchema.methods.encryptPassword = async password => {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+};
+
+UserSchema.methods.matchPassword = function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+module.exports = model("User", UserSchema);
