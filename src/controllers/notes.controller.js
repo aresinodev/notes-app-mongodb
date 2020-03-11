e034d35a43be5b9a9487385a76bce6886b69b2e3
Aarón RJ <aaron@quantumstudio.es>
2020-03-11 09:07:36 +0100

    Commit #2 11/03/2020 - Se guardan notas en la base de datos de mongoDB. - Se listan las notas.

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -5,14 +5,17 @@ notesCtrl.renderNoteForm = (req, res) => {
     res.render("notes/new-note");
 };
 
-notesCtrl.createNewNote = (req, res) => {
+notesCtrl.createNewNote = async (req, res) => {
     const { title, description } = req.body;
     const newNote = new Note({ title, description });
+    await newNote.save();
+
     res.send(newNote);
 };
 
-notesCtrl.renderNotes = (req, res) => {
-    res.send("Render notes");
+notesCtrl.renderNotes = async (req, res) => {
+    const notes = await Note.find();
+    res.render("notes/all-notes", { notes });
 }
 
 notesCtrl.renderEditForm = (req, res) => {
